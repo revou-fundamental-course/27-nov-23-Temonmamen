fd3ec24555b464451aa7510923e374a7afaaf798
Temonmamen <silverco323@gmail.com>
2023-12-05 20:31:38 +0700

    update algoritma js

--- Revou/assests/js/main.js
+++ Revou/assests/js/main.js
@@ -2,3 +2,26 @@ function show() {
     document.querySelector(".hamburger").classList.toggle("open");
     document.querySelector(".navigation").classList.toggle("active");
 }
+
+var x = document.getElementById("tinggiSeg");
+var y = document.getElementById("lebarSeg");
+var z;
+
+function clac() {
+    z = x.value * y.value / 2;  // untuk menampilkan di box hasil
+    
+    if (x.value >= 0 && y.value >= 0 ) {
+        console.log(z)
+        document.getElementById("hasilSeg").value = z;
+    }
+    
+} 
+
+function clacReset() {
+    if (z == z) {
+        document.getElementById("hasilSeg").value = "";
+        x.value = "";
+        y.value = "";
+    }
+
+}
