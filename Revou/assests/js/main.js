9815e4e0e21b4e0088a061005048e30e07b7a0b8
Temonmamen <silverco323@gmail.com>
2023-12-06 18:06:00 +0700

    final

--- Revou/assests/js/main.js
+++ Revou/assests/js/main.js
@@ -14,26 +14,50 @@ var y = document.getElementById("lebarSeg");
 var z;
 
 function clac1() {
-    z = x.value * y.value / 2;  // untuk menampilkan di box hasil
-    if (x.value >= 0 && y.value >= 0) {
-        console.log(z)
+    z = Number(x.value) * Number(y.value) / 2;  // untuk menampilkan di box hasil
+    text = "masukkan angka";
+    if (x.value > 0 && y.value > 0) {
+        console.log(z);
         document.getElementById("hasilSeg").value = z;
-    } else if (isNaN(x.value && y.value || x.value && y.value < 1 || x.value && y.value > 1)) {
-        text = "masukkan angka";
+    } else if ((x.value === "" || 0, y.value === "" || 0)) {
+        document.getElementById("alertProp1").innerHTML = text;
+        x.style.borderBlockColor = "red";
+        y.style.borderBlockColor = "red";
     }
-    document.getElementById("alertProp1").innerHTML = text;
+     else if (isNaN(x.value && y.value)) {
+        document.getElementById("alertProp1").innerHTML = text;
+        x.style.borderBlockColor = "red";
+        y.style.borderBlockColor = "red";
+    }
+    return;
 }
 
 function clacReset1() {
     if (z == z) {
         document.getElementById("hasilSeg").value = "";
+        document.getElementById("alertProp1").innerHTML = "";
+        x.style.border = "none";
+        y.style.border = "none";
         x.value = "";
         y.value = "";
-    } else if (isNaN(x.value && y.value || x.value && y.value < 1 || x.value && y.value > 1)) {
+    } else if (isNaN(x.value && y.value)) {
         document.getElementById("alertProp1").value = "";
+        document.getElementById("alertProp1").innerHTML = "";
+        x.style.border = "none";
+        y.style.border = "none";
         x.value = "";
         y.value = "";
-    }
+    } else if (isNaN(x.value < 0))  {
+        document.getElementById("hasilKel").value = "";
+        document.getElementById("alertProp1").innerHTML = "";
+        x.value = "";
+        x.style.border = "none";
+    } else if (isNaN(y.value < 0))  {
+        document.getElementById("hasilKel").value = "";
+        document.getElementById("alertProp2").innerHTML = "";
+        y.value = "";
+        y.style.border = "none";
+    } 
 
 }
 // sistem perhitungan luas segitiga end
@@ -49,29 +73,57 @@ function clac2() {
     text = "masukkan angka";
     if (a.value > 0 && b.value > 0 && c.value > 0) {
         console.log(d);
-        document.getElementById("hasilKel").value= d;
+        document.getElementById("hasilKel").value = d;
     } else if (a.value === "" || 0, b.value === "" || 0, c.value === "" || 0) {
-        console.log("masukkan angka")
+        console.log("masukkan angka");
         document.getElementById("alertProp2").innerHTML = text;
-    }
+        a.style.borderBlockColor = "red";
+        b.style.borderBlockColor = "red";
+        c.style.borderBlockColor = "red";
+    } 
     else if (isNaN(a.value && b.value && c.value)) {
         document.getElementById("alertProp2").innerHTML = text;
+        a.style.borderBlockColor = "red";
+        b.style.borderBlockColor = "red";
+        c.style.borderBlockColor = "red";
     }
-
+    return;
 }
 
 function clacReset2() {
     if (d == d) {
-        document.getElementById("hasilKel").value = ""
-        document.getElementById("alertProp2").innerHTML = ""
-        a.value = ""
-        b.value = ""
-        c.value = ""
-    } else if (isNaN(a.value && b.value && c.value || a.value && b.value && c.value < 1 || a.value && b.value && c.value > 1)) {
-        document.getElementById("hasilKel").value = ""
-        document.getElementById("alertProp2").innerHTML = ""
-        a.value = ""
-        b.value = ""
-        c.value = ""
-    }
+        document.getElementById("hasilKel").value = "";
+        document.getElementById("alertProp2").innerHTML = "";
+        a.style.border = "none";
+        b.style.border = "none";
+        c.style.border = "none";
+        a.value = "";
+        b.value = "";
+        c.value = "";
+    } else if (isNaN(a.value && b.value && c.value || a.value && b.value && c.value < 0 || a.value && b.value && c.value > 0)) {
+        document.getElementById("hasilKel").value = "";
+        document.getElementById("alertProp2").innerHTML = "";
+        a.style.border = "none";
+        b.style.border = "none";
+        c.style.border = "none";
+        a.value = "";
+        b.value = "";
+        c.value = "";
+    } else if (isNaN(a.value < 0))  {
+        document.getElementById("hasilKel").value = "";
+        document.getElementById("alertProp2").innerHTML = "";
+        a.value = "";
+        a.style.border = "none";
+    } else if (isNaN(b.value < 0))  {
+        document.getElementById("hasilKel").value = "";
+        document.getElementById("alertProp2").innerHTML = "";
+        b.value = "";
+        b.style.border = "none";
+    } else if (isNaN(c.value < 0))  {
+        document.getElementById("hasilKel").value = "";
+        document.getElementById("alertProp2").innerHTML = "";
+        c.value = "";
+        c.style.border = "none";
+    } 
 }
+// sistem perhitungan keliling segitiga end
